e385d4d1e32247ea0753d196a15297d0cf2b2a5b
LDSchaefer <45575050+LDSchaefer@users.noreply.github.com>
2019-12-09 13:41:55 +0100

    Add files via upload
    1b und 2b verbessert 2c begonnen (cmd UI)

---
+++ NeuralNet.h
@@ -0,0 +1,28 @@
+#ifndef NEUROALNET_H
+#define NEUROALNET_H
+
+#endif // NEURALNET_H
+
+#include <vector>
+#include <iostream>
+#include "Neuron.h"
+
+class NeuralNet{
+public:
+    // 2a
+    NeuralNet(const std::vector<unsigned> &topology);
+    // 2b
+    void output_all(std::vector<double> &res_vals);
+    // 2c
+    void output_spec();
+    // 2d
+    void read_in();
+    // 2e
+    void backpropagation();
+    // 2f
+    void math_dist();
+private:
+    std::vector<Layer> net_struct; // Die Strucktur unseres Netzes,
+                                   //Zugriff auf einzelne Neuronen/Vektoren: net_struct[layer_num][neuron_num]
+
+};
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+Erster Versuch ein Neurales Netzwerk aus mehreren Neuronen aus 1 zusammen zusetzen
+*/
+#include <iostream>
+#include <vector>
+#include "NeuralNet.h"
+
+
+int main()
+{
+    std::vector<unsigned> topology;
+    topology.push_back(1); //input für Topologie größe
+    topology.push_back(2); //input für Topologie größe
+    topology.push_back(3); //input für Topologie größe
+    NeuralNet net(topology);
+
+    return 0;
+}
+
+// Als Referenz: https://www.youtube.com/watch?v=KkwX7FkLfug
